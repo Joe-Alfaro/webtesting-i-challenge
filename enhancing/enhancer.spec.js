48668c74d00c0782d567b177b6b668ea40067b15
joe-alfaro <josephlalfaro@gmail.com>
2019-05-13 12:25:09 -0700

    add functionality and testing for enhancer repair method

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,23 @@
 const enhancer = require('./enhancer.js');
-// test away!
+const {succeed, fail, repair, get} = require('./enhancer.js');
+
+describe('enhancer.js', () => {
+  describe('repair() method', () => {
+    it('should return a new item with a durability of 100 when passed an item', () => {
+      const item = {};
+      expect(repair(item).durability).toBe(100);
+      item.durability = 20;
+      expect(repair(item).durability).toBe(100);
+    })
+  });
+
+  describe('success() method', () => {
+
+  });
+  describe('fail() method', () => {
+
+  });
+  describe('get() method', () => {
+
+  });
+});
